615d1084c0234e57a126126e098bf00fea0519a9
rifan <blockchainlover2019@gmail.com>
2021-09-18 08:47:12 +0800

    mdex

--- src/services/mdex/helpers.js
+++ src/services/mdex/helpers.js
@@ -25,12 +25,16 @@ export const getUserPools = async (acc) => {
           let lp_address = lpAddresses[i][0];
           let lpContract = new web3.eth.Contract(config.mdexLPTokenABI, lp_address);
           console.log(lp_address);
+          
           let token0Address = await lpContract.methods.token0().call();
           let token1Address = await lpContract.methods.token1().call();
+
           let token0Contract = new web3.eth.Contract(config.erc20ABI, token0Address);
           let token0Symbol = await token0Contract.methods.symbol().call();
+          
           let token1Contract = new web3.eth.Contract(config.erc20ABI, token1Address);
           let token1Symbol = await token1Contract.methods.symbol().call();
+          
           let pool_info = {
             name: token0Symbol + "/" + token1Symbol + " Pool",
             symbol: token0Symbol + "/" + token1Symbol,
